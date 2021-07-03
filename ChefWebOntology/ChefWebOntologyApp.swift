17fb057039e4f490c412394ed4ef963d59875cef
Monika Mateska <monika.mateska@netcetera.com>
2021-07-03 18:25:29 +0200

    Initial Commit

---
+++ ChefWebOntology/ChefWebOntologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefWebOntologyApp.swift
+//  ChefWebOntology
+//
+//  Created by Monika Mateska on 3.7.21.
+//
+
+import SwiftUI
+
+@main
+struct ChefWebOntologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
